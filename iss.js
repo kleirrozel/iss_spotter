ffc78f06b2ee3732d56c46f30d7e914819ffb66b
Kleir Miranda <kleirmiranda@gmail.com>
2021-07-01 18:46:47 -0400

    Added the fetchCoordsByIP function to fetch the latitude and longitude of the IP address

--- iss.js
+++ iss.js
@@ -6,18 +6,44 @@ const fetchMyIP = function(callback) {
       // error = `${error}`;
       callback(error, null);
       // console.log(error);
+      return;
     }
     if (response && response.statusCode !== 200) {
       // error = `${response && response.statusCode}`;
       callback(error(`Status Code: ${response.statusCode} when fetching IP address: ${body}.`), null);
       // console.log(`${response && response.statusCode}`);
+      return;
     }
     if (body) {
       // console.log(body)
-      const ip = JSON.parse(body);
-      callback(null, ip);
+      const ipCoordinates = JSON.parse(body);
+      callback(null, ipCoordinates);
+      return;
     }
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response && response.statusCode !== 200) {
+      const message = `Status Code: ${response.statusCode} when fetching coordinates for your IP. Response: ${body}.`;
+      callback(message, null);
+      return;
+    }
+
+    if (body) {
+      const data = JSON.parse(body);
+      const coordinates = { "latitude": data.latitude, 
+                            "longitude": data.longitude}
+      callback(null, coordinates);
+      return;
+    }
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
